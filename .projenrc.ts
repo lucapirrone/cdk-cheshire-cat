b6fb1e69ad375346dce8ebcf664a5cbf573080d0
Luca Pirrone <lpirrone2000@gmail.com>
2024-01-16 23:53:43 +0100

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,17 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Luca Pirrone',
+  authorAddress: 'lpirrone2000@gmail.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.0.0',
+  name: 'cdk-cheshire-cat',
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/lpirrone2000/cdk-cheshire-cat.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
